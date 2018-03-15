62a8774e5f29f9a410a7fe39ecabd9c2eb7ff246
kecman88 <34206025+kecman88@users.noreply.github.com>
2018-03-15 19:25:06 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('It works');
+});
