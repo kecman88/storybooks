d95b0452c5007a490d4cc3488e519dc099619511
kecman88 <34206025+kecman88@users.noreply.github.com>
2018-03-15 20:17:50 +0100

    Google oAuth

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const app = express();
-
 const port = process.env.PORT || 5000;
+
+// Passport config
+require('./config/passport')(passport);
+
+// Load routes
+const auth = require('./routes/auth');
+// Use routes
+app.use('/auth', auth);
+
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
+
+
 
 app.get('/', (req, res) => {
     res.send('It works');
