699d65a90c071f1f433459df4a31798bb02335af
kecman88 <34206025+kecman88@users.noreply.github.com>
2018-03-17 11:52:01 +0100

    First layouts

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+
 const keys = require('./config/keys');
 
 
@@ -29,8 +31,21 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Handlebars middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 // Load routes
 const auth = require('./routes/auth');
+const index = require('./routes/index');
+
+// Set global variables
+app.use((req, res, next) => {
+    res.locals.user = req.user || null;
+    next();
+});
 
 // Map global promises
 mongoose.Promise = global.Promise;
@@ -43,11 +58,9 @@ mongoose.connect(keys.mongoURI)
 
 // Use routes
 app.use('/auth', auth);
+app.use('/', index);
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
 
-app.get('/', (req, res) => {
-    res.send('It works');
-});
