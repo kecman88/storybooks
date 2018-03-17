9c3c5504cec782352077f6bce2bffce800523b8b
kecman88 <34206025+kecman88@users.noreply.github.com>
2018-03-17 10:03:56 +0100

    Login feature

--- app.js
+++ app.js
@@ -1,24 +1,52 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const keys = require('./config/keys');
 
-const app = express();
+
 const port = process.env.PORT || 5000;
+
+// Initialize app
+const app = express();
+
+// Load User model
+require('./models/User');
 
 // Passport config
 require('./config/passport')(passport);
 
+// Session middleware
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Load routes
 const auth = require('./routes/auth');
+
+// Map global promises
+mongoose.Promise = global.Promise;
+
+// Mongoose connect
+mongoose.connect(keys.mongoURI)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+
 // Use routes
 app.use('/auth', auth);
-
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
-
-
 
 app.get('/', (req, res) => {
     res.send('It works');
--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,9 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('./keys');
 
+// Load User model
+const User = mongoose.model('users');
+
 module.exports = function(passport) {
     passport.use(
         new GoogleStrategy({
@@ -10,9 +13,45 @@ module.exports = function(passport) {
             callbackURL: "/auth/google/callback",
             proxy: true
         }, (accessToken, refreshToken, profile, done) => {
-            console.log(accessToken);
-            console.log(profile);
+            const imageStr = profile.photos[0].value;
+            const image = imageStr.substring(0, imageStr.indexOf('?'));
+            
+            const newUser = {
+                googleID: profile.id,
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                email: profile.emails[0].value,
+                image: image
+            }
+
+            // Check for existing user
+            User.findOne({
+                googleID: profile.id
+            }).then(user => {
+                if (user) {
+                    done(null, user);
+                } else {
+                    // Create User
+                    new User(newUser)
+                        .save()
+                        .then(user => {
+                            done(null, user);
+                        })
+
+                }
+            })
+
         })
-    )
+    );
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+      
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
 
 }
